e58e025787f556ef78a195e1e2aa960db508ffc5
wahidnur659 <wahidnurrohim@students.amikom.ac.id>
2025-06-09 10:23:43 +0700

    Add script to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 
 <script setup>
 import MainLayout from '@/layout/MainLayout.vue'
+import HelloAPI from './components/HelloAPI.vue'
 </script>
 
 <template>
   <MainLayout />
+  <HelloAPI />
 </template>
 
 <style scoped>
