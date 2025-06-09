41fa4511b5e33b1ab3b317f0245eb48e12654a49
wahidnur659 <wahidnurrohim@students.amikom.ac.id>
2025-06-09 10:01:56 +0700

    connect 2 repo

---
+++ src/components/HelloAPI.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  try {
+    const res = await axios.get('http://127.0.0.1:8000/api/hello')
+    message.value = res.data.message
+  } catch (error) {
+    message.value = 'Gagal ambil data dari backend'
+  }
+})
+</script>
